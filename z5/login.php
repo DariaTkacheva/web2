778562a5409013e313ffd973b6fb5f1b72d30031
DariaTkacheva <90569700+DariaTkacheva@users.noreply.github.com>
2022-05-15 11:34:19 +0300

    Update login.php

--- z5/login.php
+++ z5/login.php
@@ -1,13 +1,21 @@
-<?php
+<?php/**
+ * Файл login.php для не авторизованного пользователя выводит форму логина.
+ * При отправке формы проверяет логин/пароль и создает сессию,
+ * записывает в нее логин и id пользователя.
+ * После авторизации пользователь перенаправляется на главную страницу
+ * для изменения ранее введенных данных.
+ **/
 
-header('Content-Type: text/html; charset=UTF-8');
+header('Content-Type: text/html; charset=UTF-8');// Отправляем браузеру правильную кодировку,
+// файл login.php должен быть в кодировке UTF-8 без BOM.
 
-session_start();
+session_start();// Начинаем сессию.
 
 $db_user = 'u47541';
 $db_pass = '8900409';
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
     if(isset($_GET['do'])&&$_GET['do'] == 'logout'){
     session_start();    
     session_unset();
@@ -25,11 +33,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 <?php
 }
-else {
+else {// Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
 
-  $login = $_POST['login'];
+  $login = $_POST['login'];  // Если все ок, то авторизуем пользователя.
   $pass =  $_POST['pass'];
-
+// TODO: Проверть есть ли такой логин и пароль в базе данных.
+  // Выдать сообщение об ошибках.
   $db = new PDO('mysql:host=localhost;dbname=u47541', $db_user, $db_pass, array(
     PDO::ATTR_PERSISTENT => true
   ));
@@ -53,5 +62,5 @@ else {
     echo 'Ошибка: ' . $e->getMessage();
     exit();
   }
-  header('Location: ./');
+  header('Location: ./'); // Делаем перенаправление.
 }
